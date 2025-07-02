41b2cb400992455030a592b1b821bcf8524771fa
SeeniselvamB <bseeniselvam2006@gmail.com>
2025-07-02 16:33:09 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,66 +1,5 @@
-// import React , { useEffect } from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css';
-// import Navbar from './Navbar';
-// import Home from './Home';
-// import About from './About';
-// import Timing from './Timing';
-// import Events from './Branch';           // ✅ Your Branch component here
-// import Images from './Images';
-// import Location from './Location';
-// import GalleryPage from './GalleryPage';
-// import HistoryPage from './HistoryPage';
-// import TamilHistory from './TamilHistory';
-// import Media from './Media';
-// import BranchDetail from './BranchDetail';
-// // import Father from './Father';
-// // import FatherHistory from './FatherHistory';  // ✅ Import detail page
-// import './App.css';
-
-
-// function App() {
-//   useEffect(() => {
-//     AOS.init({ duration: 1000, once: true });
-//   }, []);
-//   return (
-//     <Router basename='/Church'>
-//       <div className="App">
-//         <Navbar />
-//         <Routes>
-//           <Route
-//             path="/"
-//             element={
-//               <>
-//                 <section id="home"><Home /></section>
-//                 <section id="about"><About /></section>
-//                 {/* <section id="father"><Father /></section> */}
-//                 <section id="timing"><Timing /></section>
-//                 <section id="gallery"><Images /></section>
-//                 <section id="events"><Events /></section>  {/* ✅ This is Branch list */}
-//                 <section id="media"><Media /></section>
-//                 <section id="location"><Location /></section>
-//               </>
-//             }
-//           />
-//           <Route path="/gallery" element={<GalleryPage />} />
-//           <Route path="/history" element={<HistoryPage />} />
-//           <Route path="/history/tamil" element={<TamilHistory />} />
-//           <Route path="/branch/:id" element={<BranchDetail />} />  {/* ✅ Detail view route */}
-//           {/* <Route path="/fathers/history" element={<FatherHistory />} /> */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
 import Navbar from './Navbar';
 import Home from './Home';
 import About from './About';
@@ -79,12 +18,8 @@ import BranchDetail from './BranchDetail';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    AOS.init({ duration: 1000, once: true });
-  }, []);
-
-  return (
-    <Router basename={process.env.REACT_APP_BASENAME || '/'}>
+    return (
+    <Router>
       <div className="App">
         <Navbar />
         <Routes>
